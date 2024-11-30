ad1856c517f2ca30a5b713096aa4781edbe1cdb9
criptomaniapp <criptomania.app@gmail.com>
2024-11-29 21:19:59 -0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { WalletProvider } from "@/context/WalletContext"; // Importar el proveedor del contexto
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WalletProvider } from '@/context/WalletContext';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function app({ Component, pageProps }: AppProps) {
   return (
     <WalletProvider>
       <Component {...pageProps} />
     </WalletProvider>
   );
 }
+
 
