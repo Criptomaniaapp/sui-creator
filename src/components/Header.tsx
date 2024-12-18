11749e6a36cab89ceba546302873da43b5f2776e
criptomaniapp <criptomania.app@gmail.com>
2024-12-18 14:32:29 -0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
+
 
 export default function Header() {
   const [network, setNetwork] = useState('Mainnet'); // Default network
