f02cc5a1452a811673eaf3f8e06c0eb9734f1ed8
criptomaniapp <criptomania.app@gmail.com>
2024-12-17 17:08:51 -0700

    update

---
+++ src/tests/suiClientTest.js
@@ -0,0 +1,13 @@
+import { client } from '../utils/suiClient.js';
+
+async function testClientConnection() {
+    try {
+        const version = await client.getRpcApiVersion();
+        console.log('Conexión exitosa. Versión del RPC:', version);
+    } catch (error) {
+        console.error('Error probando la conexión al cliente:', error);
+    }
+}
+
+testClientConnection();
+
